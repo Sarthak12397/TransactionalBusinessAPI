74fdfe8bc82c47a4d98c3d9e9f3b86073747e7e7
Sarthak12397 <ssarthak737@gmail.com>
2026-04-04 11:51:19 +0545

    Remove password from appsettings.json - security fix

--- Program.cs
+++ Program.cs
@@ -46,7 +46,11 @@ builder.Services.AddScoped<StuckTransactionRecoveryJob>();
 
 
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PaymentDbContext>();
+    db.Database.Migrate();
+}
 app.UseHttpsRedirection();
 
 app.UseExceptionHandler(errorApp =>
