7ffa21b7e52eae47441b19e56767229bfd449d34
Sarthak12397 <ssarthak737@gmail.com>
2026-04-05 18:39:23 +0545

    Fix test project, correlation ID, final cleanup

--- Program.cs
+++ Program.cs
@@ -2,9 +2,30 @@ using Microsoft.EntityFrameworkCore;
 using TransactionalBusiness.Api.Data;
 using TransactionalBusiness.Api.Services;
 using System.Text.Json.Serialization;
+<<<<<<< Updated upstream
 
 var builder = WebApplication.CreateBuilder(args);
 
+=======
+using Hangfire;
+using Hangfire.PostgreSql;
+using TransactionalBusiness.Api.Jobs;
+using CorrelationId;
+using CorrelationId.DependencyInjection;
+using Serilog;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/payment-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+builder.Services.AddHangfireServer();
+builder.Host.UseSerilog();
+builder.Services.AddDefaultCorrelationId();
+
+>>>>>>> Stashed changes
 builder.Services.AddControllers()
     .AddJsonOptions(options => {
         options.JsonSerializerOptions.Converters
@@ -20,7 +41,17 @@ builder.Services.AddDbContext<PaymentDbContext>(
 builder.Services.AddScoped<ITransactionService, TransactionService>();
 
 var app = builder.Build();
+<<<<<<< Updated upstream
 
+=======
+app.UseCorrelationId();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PaymentDbContext>();
+    db.Database.Migrate();
+}
+>>>>>>> Stashed changes
 app.UseHttpsRedirection();
 
 app.UseExceptionHandler(errorApp =>
